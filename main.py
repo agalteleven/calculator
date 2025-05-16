4a8ad1b3af1273eb78204e8b13ff518f2e1684ee
Your Name <you@example.com>
2025-05-16 15:47:07 -0700

    Move contense

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+sc = turtle.Screen()
+sc.setup(width=600, height=400)  
+sc.bgcolor("cyan")
+
+
+firstnumber = turtle.textinput("First Number", "first number")
+
+operation = turtle.textinput("operation", "multiply*, divide/, subtract-, add +")
+
+secondnumber = turtle.textinput("second number", "second number")
+
+
