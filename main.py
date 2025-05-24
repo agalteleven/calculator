3b9fa70479bfb4c0345da4878c4a25eedeb7a82c
Mastercoder1323 <sparlevi18@gmail.com>
2025-05-24 16:15:36 -0700

    Move your code

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import turtle
-from libload import lib
 
-lib.hello_rust()
 sc = turtle.Screen()
-sc.setup(width=600, height=400)  
+sc.setup(width=600, height=400)
 sc.bgcolor("cyan")
-
+t = turtle.Turtle()
 
 firstnumber = turtle.textinput("First Number", "first number")
 
@@ -13,4 +11,58 @@ operation = turtle.textinput("operation", "multiply*, divide/, subtract-, add +"
 
 secondnumber = turtle.textinput("second number", "second number")
 
+if "+" in operation:
+    answer = float(firstnumber) + float(secondnumber)
+    print(answer)
+    t.write("answer: " + str(answer), font=("Arial", 30, "normal"))
+    
+    
+if "-" in operation:
+    answer = float(firstnumber) - float(secondnumber)
+    print(answer)
+    t.write("answer: " + str(answer), font=("Arial", 30, "normal"))
+    
+if "*" in operation:
+    answer = float(firstnumber) * float(secondnumber)
+    print(answer)
+    t.write("answer: " + str(answer), font=("Arial", 30, "normal"))
+    
+if "/" in operation:
+    answer = float(firstnumber) / float(secondnumber)
+    print(answer)
+    t.write("answer: " + str(answer), font=("Arial", 30, "normal"))
+    
+secondcalculation = turtle.textinput("would you like to go again", "would you like to go again")
 
+if "yes" in secondcalculation:
+    t.clear()
+    firstnumber = turtle.textinput("First Number", "first number")
+
+    operation = turtle.textinput("operation", "multiply*, divide/, subtract-, add +")
+
+    secondnumber = turtle.textinput("second number", "second number")
+
+    if "+" in operation:
+        answer = float(firstnumber) + float(secondnumber)
+        print(answer)
+        t.write("answer: " + str(answer), font=("Arial", 30, "normal"))
+    
+    
+    if "-" in operation:
+        answer = float(firstnumber) - float(secondnumber)
+        print(answer)
+        t.write("answer: " + str(answer), font=("Arial", 30, "normal"))
+    
+    if "*" in operation:
+        answer = float(firstnumber) * float(secondnumber)
+        print(answer)
+        t.write("answer: " + str(answer), font=("Arial", 30, "normal"))
+    
+    if "/" in operation:
+        answer = float(firstnumber) / float(secondnumber)
+        print(answer)
+        t.write("answer: " + str(answer), font=("Arial", 30, "normal"))
+    t.clear()
+    t.write("no more free tryal", font=("Arial", 30, "normal"))
+    
+    
